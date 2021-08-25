db6b9f0f7dc4f22d6963ad1c89779085438bf3d2
PawZet <pawelzurawski898@gmail.com>
2021-08-25 23:51:41 +0200

    1_commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,3 @@
-import * as model from './model.js';
-import icons from 'url:../img/icons.svg';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import recipeView from './views/recipeView.js';
-
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -17,34 +11,3 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-
-const renderSpinner = function name(parentEl) {
-  const markup = ` 
-      <div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div>`;
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-};
-
-const showRecipe = async function () {
-  try {
-    const id = window.location.hash.slice(1);
-    //Loading recipe
-
-    if (!id) return;
-    renderSpinner(recipeContainer);
-    await model.loadRecipe(id);
-    //Rendering recipe
-    recipeView.render(model.state.recipe);
-  } catch (err) {
-    alert(err);
-  }
-};
-
-// window.addEventListener('hashchange', showRecipe);
-// window.addEventListener('load', showRecipe);
-
-['hashchange', 'load'].forEach(e => window.addEventListener(e, showRecipe));
