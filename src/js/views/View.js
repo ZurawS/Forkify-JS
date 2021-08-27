d4858825009891cd61d7005f5e2be7e4e503ff67
PawZet <pawelzurawski898@gmail.com>
2021-08-27 12:29:09 +0200

    commit_6-bookmarks

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -11,6 +11,40 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+    // console.log(curElements);
+    // console.log(newElements);
+
+    //Method which swaps only elements which were changed instead of reloading whole page
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
+
+      //Choose only content inside new element that contains changed text and trim white spaces
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        // console.log(newEl.firstChild?.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+
+      //Updates changed ATTRIBUTE
+      if (!newEl.isEqualNode(curEl)) {
+        Array.from(newEl.attributes).forEach(attr => {
+          curEl.setAttribute(attr.name, attr.value);
+        });
+        // console.log(newEl.attributes);
+      }
+    });
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
